3ec1e583c7b853a469a2f98f340d950d96011abd
Dominique Lederer <dominique.lederer@return1.at>
2012-01-30 17:41:53 +0100

    first commit

---
+++ init.rb
@@ -0,0 +1,23 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_hide_estimated_hours do
+  name 'Hide estimated hours'
+  author 'Dominique Lederer (return1)'
+  description 'This Redmine plugin reuses the "view_time_entries" permission to hide the estimated hours field'
+  version '1.0.0'
+  url 'http://return1.at/'
+  author_url 'http://return1.at/'
+
+  requires_redmine :version_or_higher => '1.3.0'
+
+end
+
+require 'dispatcher'
+Dispatcher.to_prepare :redmine_hide_estimated_hours do
+  require_dependency 'query'
+  unless Query.included_modules.include? RedmineHideEstimatedHours::Patches::QueryPatch
+    Query.send(:include, RedmineHideEstimatedHours::Patches::QueryPatch)
+  end
+end
+
+require 'redmine_hide_estimated_hours/hooks/helper_issues_show_detail_after_setting_hook'
---
+++ lib/redmine_hide_estimated_hours/hooks/helper_issues_show_detail_after_setting_hook.rb
@@ -0,0 +1,13 @@
+module RedmineHideEstimatedHours
+  module Hooks
+    class HelperIssuesShowDetailAfterSettingHook < Redmine::Hook::ViewListener
+      def helper_issues_show_detail_after_setting(context = { })
+        # setting the value to an empty string causes redmine to fake a deleted entry
+        if context[:detail].prop_key == 'estimated_hours' && !User.current.allowed_to?(:view_time_entries, context[:project])
+          context[:detail].value = ""
+        end
+        ''
+      end
+    end
+  end
+end
