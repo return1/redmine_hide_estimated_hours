10a491755016c785b60d6b6e3c9dad158cef8d23
Dominique Lederer <dominique.lederer@return1.at>
2015-04-08 12:29:56 +0200

    fixing email journal

--- lib/journal_patch.rb
+++ lib/journal_patch.rb
@@ -20,7 +20,7 @@ module JournalPatch
         elsif detail.property == 'relation'
           Issue.find_by_id(detail.value || detail.old_value).try(:visible?, user)
         # removing estimated time in this elsif
-        elsif detail.property == 'attr' && detail.prop_key == 'estimated_hours' && !User.current.allowed_to?(:view_time_entries, project)
+        elsif detail.property == 'attr' && detail.prop_key == 'estimated_hours' && !user.allowed_to?(:view_time_entries, project)
           false
         else
           true
