ef2daeefd7c1b512e6ca07663032110258184aa3
Dominique Lederer <dominique.lederer@return1.at>
2021-01-07 13:58:11 +0100

    4.1.1 readme

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_hide_estimated_hours do
   name 'Hide estimated hours'
   author 'Dominique Lederer (return1)'
   description 'This Redmine plugin reuses the "view_time_entries" permission to hide the estimated hours field'
-  version '1.0.12'
+  version '1.0.13'
   url 'https://github.com/return1/redmine_hide_estimated_hours'
   author_url 'http://return1.at/'
 
